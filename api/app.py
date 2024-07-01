98f7552cca147447c2659d5c7029890568cb6dbe
Folafolu <ofolafolu@gmail.com>
2024-07-01 21:50:56 +0100

    fixes

--- api/app.py
+++ api/app.py
@@ -6,23 +6,26 @@ app = Flask(__name__)
 import requests
 
 def get_ip():
-    response = requests.get('https://ipinfo.io/json').json()
+    # response = requests.get('https://ipinfo.io/json').json()
+    ip = request.remote_addr
+    response = requests.get(f'https://ipapi.co/{ip}/json/').json()
     client_ip = response["ip"]
     city = response["city"]
-    lon, lat = response["loc"].split(",")
-    return client_ip, city, lon, lat
+    lat = response["latitude"]
+    lon = response["longitude"]
+    return client_ip, city, lat, lon
 
 
-def get_temperature(city):
-    temp_resp = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?unitGroup=metric&include=current&key=XCN2PGTTQUNJHS3YALETAUUYA&contentType=json').json()
+def get_temperature(lat, lon):
+    temp_resp = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{lat}%2C%20{lon}?unitGroup=metric&key=XCN2PGTTQUNJHS3YALETAUUYA&contentType=json').json()
     return temp_resp['currentConditions']['temp']
 
 
 @app.route('/api/hello', methods=["get"])
 def hello():
     visitor_name = request.args.get('visitor_name')
-    client_ip, city, lon, lat = get_ip()
-    temperature = get_temperature(city)
+    client_ip, city, lat, lon = get_ip()
+    temperature = get_temperature(lat, lon)
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {city}"
     response = {
         "client_ip": client_ip,
